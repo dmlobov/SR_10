ebee0ee51da9c78c57139ca784c94d0cabbcc865
dmlob <dmlob@DESKTOP-0VEH751>
2022-11-26 23:50:36 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int gradus = Convert.ToInt32(Console.ReadLine());
+            int minute = Convert.ToInt32(Console.ReadLine());
+            int second = Convert.ToInt32(Console.ReadLine());
+            Ugol ugol = new Ugol(gradus, minute, second);
+            double ugolRadians = ugol.ToRadians();
+            Console.WriteLine("{0:0.0000}", ugolRadians);
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Task1/Ugol.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Ugol
+    {
+       int gradus;
+       int minute;
+       int second;
+        public int Gradus
+        {
+            get
+            {
+                return gradus;
+            }
+            set
+            {
+                gradus = value % 360;
+            }
+        }
+        public int Minute
+        {
+            get
+            {
+                return minute;
+            }
+            set
+            {
+                Gradus += value / 60;
+                minute = value % 60;
+            }
+        }
+        public int Second
+        {
+            get
+            {
+                return second;
+            }
+            set
+            {
+                Minute += value / 60;
+                second = value % 60;
+            }
+        }
+        public Ugol(int gradus, int minute, int second)
+        {
+            this.Gradus = gradus;
+            this.Minute = minute;
+            this.Second = second;
+        }
+
+
+        public double ToRadians()
+        {
+            return ((((double)second) / 3600 + ((double)minute) / 60 + gradus) / 180) * Math.PI;
+        }
+    }
+}
